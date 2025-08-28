5a8c50dbd09272a37dfe9b447488fd3a2c701527
marcosvenancio20 <Marcosfilhov20@gmail.com>
2025-08-28 17:55:36 -0300

    Animação run

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_FORCE = -400.0
 
-
+@onready var animation :=$AnimationPlayer/piscar_mochi
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,6 +19,7 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
