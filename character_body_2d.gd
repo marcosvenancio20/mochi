61d1b3422b828ad287adb2af4b755cb9bb3fe3cf
marcosvenancio20 <Marcosfilhov20@gmail.com>
2025-09-16 18:25:29 -0300

    Cenario com movimento

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_FORCE = -400.0
-
-@onready var animation :=$AnimationPlayer/piscar_mochi
+const SPEED = 230.0
+const JUMP_FORCE = -350.0
+var is_jumping := false
+@onready var animation :=$anim as AnimatedSprite2D
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -13,14 +13,23 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
-
+		is_jumping=true
+	elif is_on_floor():
+		is_jumping=false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction  !=0:
 		velocity.x = direction * SPEED
-		animation.play("run")
+		animation.scale.x=direction                                                                                
+		
+		if !is_jumping:
+			animation.play("run")
+	elif is_jumping:
+		animation.play("jump")
+			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("default")
 
 	move_and_slide()
