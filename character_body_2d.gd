35bd404d9f89d41784f513917e73dd8a9db497f8
marcosvenancio20 <Marcosfilhov20@gmail.com>
2025-10-07 17:02:48 -0300

    enemy

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 100.0
 const JUMP_FORCE = -300.0
 var is_jumping := false
 @onready var animation :=$anim as AnimatedSprite2D
---
+++ enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 2000.0
+@onready var texture:=$trexture as Sprite2D
+@onready var wall_detector :=$wall_detector as RayCast2D
+var direction:=-1
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	
+
+	
+	if wall_detector.is_colliding():
+		direction*=1
+		wall_detector.scale.x  *=-1
+	
+	if direction ==1:
+		texture.flip_h=true
+	else:
+		texture.flip_h=false
+		
+	velocity.x = direction * SPEED* delta
+	move_and_slide()
